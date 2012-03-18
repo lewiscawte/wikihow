100194d568a4d95ed259355121687d89592e1762
Jack Phoenix <jack@countervandalism.net>
2012-03-18 15:39:58 +0000

    AdminMarkEmailConfirmed: like r21, just for this extension

--- wikiHow/extensions/AdminMarkEmailConfirmed/AdminMarkEmailConfirmed.body.php
+++ wikiHow/extensions/AdminMarkEmailConfirmed/AdminMarkEmailConfirmed.body.php
@@ -1,22 +1,23 @@
-<?
+<?php
 
-if (!defined('MEDIAWIKI')) die();
+class AdminMarkEmailConfirmed extends SpecialPage {
 
-class AdminMarkEmailConfirmed extends UnlistedSpecialPage {
-
-	function __construct() {
-		UnlistedSpecialPage::UnlistedSpecialPage('AdminMarkEmailConfirmed');
+	/**
+	 * Constructor -- set up the new special page
+	 */
+	public function __construct() {
+		parent::__construct( 'AdminMarkEmailConfirmed', 'adminmarkemailconfirmed' );
 	}
 
 	/**
 	 * Confirm a user's email address (account found by username).
 	 *
-	 * @param $username string, the username
-	 * @return their email address
+	 * @param $username String: the username
+	 * @return String: their e-mail address, if any
 	 */
-	function confirmEmailAddress($username) {
-		$user = User::newFromName($username);
-		if ($user && $user->getID() > 0) {
+	function confirmEmailAddress( $username ) {
+		$user = User::newFromName( $username );
+		if ( $user && $user->getID() > 0 ) {
 			$user->confirmEmail();
 			$emailAddr = $user->getEmail();
 			return $emailAddr;
@@ -26,77 +27,75 @@ class AdminMarkEmailConfirmed extends UnlistedSpecialPage {
 	}
 
 	/**
-	 * Execute special page.  Only available to wikihow staff.
+	 * Show the special page
+	 *
+	 * @param $par Mixed: parameter passed to the special page or null
 	 */
-	function execute() {
+	public function execute( $par ) {
 		global $wgRequest, $wgOut, $wgUser, $wgLang;
 
-		$userGroups = $wgUser->getGroups();
-		if ($wgUser->isBlocked() || !in_array('staff', $userGroups)) {
-			$wgOut->setRobotpolicy('noindex,nofollow');
-			$wgOut->errorpage('nosuchspecialpage', 'nospecialpagetext');
+		// Check permissions
+		if ( !$wgUser->isAllowed( 'adminmarkemailconfirmed' ) ) {
+			$this->displayRestrictionError();
 			return;
 		}
 
-		if ($wgRequest->wasPosted()) {
-			$username = $wgRequest->getVal('username', '');
-			$wgOut->setArticleBodyOnly(true);
-			$emailAddr = $this->confirmEmailAddress($username);
-			if ($emailAddr) {
-				$tmpl = <<<EOHTML
-<p>Account '{$username}' email address has been confirmed. No email has been sent to the user.</p>
-<p>Their email address: $emailAddr</p>
-EOHTML;
-				$result = array('result' => $tmpl);
+		// Show a message if the database is in read-only mode
+		if ( wfReadOnly() ) {
+			$wgOut->readOnlyPage();
+			return;
+		}
+
+		// If the user is blocked, they don't need to access this page
+		if ( $wgUser->isBlocked() ) {
+			$wgOut->blockedPage();
+			return;
+		}
+
+		// The JS file POSTs into this very page
+		if ( $wgRequest->wasPosted() ) {
+			$username = $wgRequest->getVal( 'username', '' );
+
+			// So that we don't get a special page inside a special page :P
+			// We're not interested in the skin, we just need the content
+			$wgOut->setArticleBodyOnly( true );
+
+			$emailAddr = $this->confirmEmailAddress( $username );
+
+			if ( $emailAddr ) {
+				$tmpl = wfMessage( 'adminmarkemailconfirmed-success', $username, $emailAddr )->parseAsBlock();
+				$result = array( 'result' => $tmpl );
 			} else {
-				$result = array('result' => "error: user '{$username}' not found");
+				$result = array(
+					'result' => wfMsg( 'adminmarkemailconfirmed-error', $username )
+				);
 			}
-			print json_encode($result);
+			echo json_encode( $result );
 			return;
 		}
 
-		$wgOut->setHTMLTitle('Admin - Confirm User Email Address - wikiHow');
+		// Set headers, such as the page title, robot policy, etc.
+		$this->setHeaders();
 
-$tmpl = <<<EOHTML
-<form method="post" action="/Special:AdminMarkEmailConfirmed">
-<h4>Enter username of email address to confirm</h4>
-<br/>
+		$instructions = wfMsg( 'adminmarkemailconfirmed-instructions' );
+		$confirm = wfMsg( 'adminmarkemailconfirmed-confirm-button' );
+		$postURL = $this->getTitle()->getFullURL();
+		$tmpl = <<<EOHTML
+<form method="post" action="$postURL">
+<h4>$instructions</h4>
+<br />
 <input id="action-username" type="text" size="40" />
-<button id="action-go" disabled="disabled">confirm</button><br/>
-<br/>
+<button id="action-go" disabled="disabled">$confirm</button><br/>
+<br />
 <div id="action-result">
 </div>
 </form>
-
-<script>
-(function($) {
-	$(document).ready(function() {
-		$('#action-go')
-			.attr('disabled', '')
-			.click(function () {
-				$('#action-result').html('loading ...');
-				$.post('/Special:AdminMarkEmailConfirmed',
-					{ 'username': $('#action-username').val() },
-					function(data) {
-						$('#action-result').html(data['result']);
-						$('#action-username').focus();
-					},
-					'json');
-				return false;
-			});
-		$('#action-username')
-			.focus()
-			.keypress(function (evt) {
-				if (evt.which == 13) { // if user hits 'enter' key
-					$('#action-go').click();
-					return false;
-				}
-			});
-	});
-})(jQuery);
-</script>
 EOHTML;
 
-		$wgOut->addHTML($tmpl);
+		// Add JS
+		$wgOut->addModules( 'ext.adminMarkEmailConfirmed' );
+
+		// Output the form
+		$wgOut->addHTML( $tmpl );
 	}
 }
