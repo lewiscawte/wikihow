c776fdc4976f38ad53e0bd454ad2ba0c71f30028
Jack Phoenix <jack@countervandalism.net>
2012-03-18 01:54:06 +0000

    FixCaps: hopefully make this a bit more self-contained and not require a core hack or two in order to work.
    In the 12 March 2012 release, the following files have references to "fixcaps" and may need to be changed:
    *extensions/wikihow/editpagewrapper.css
    *extensions/wikihow/popupEdit.js
    *skins/WikiHow/editpagewrapper.css
    *skins/WikiHow/new.css
    *skins/WikiHow/popupEdit.js

--- wikiHow/extensions/FixCaps/FixCaps.php
+++ wikiHow/extensions/FixCaps/FixCaps.php
@@ -1,5 +1,39 @@
-<?
+<?php
+/**
+ * An extension that adds a new button, which allows to change the
+ * capitalization of the page text, to the edit toolbar.
+ *
+ * Functionality is provided here, styling is done elsewhere.
+ *
+ * @file
+ * @ingroup Extensions
+ * @version 1.0
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
 
-function wfGetFixCapsButton() {
-	return "<div style='float:left;'><input id='fixcaps_button' type='image' src='/extensions/wikihow/fixcaps.png' accesskey='o' onclick='javascript:fixcaps(document.editform.wpTextbox1)'/></div>\n"  ;
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die();
+}
+
+$wgHooks['EditPageBeforeEditToolbar'][] = 'wfFixCaps';
+
+function wfFixCaps( &$toolbar ) {
+	global $wgExtensionAssetsPath;
+
+	// Inline JS, moved from skins/common/clientscript.js, because I'm too lazy
+	// to figure out how to make ResourceLoader work properly in an environment
+	// like this.
+	$toolbar .= <<<JS
+function fixcaps( e ) {
+	var text = e.value.toLowerCase().replace( /(^\s*\w|[\.\!\?]\s*\w)/g, function( c ) { return c.toUpperCase(); } );
+	text = text.replace( /(^(#|\*)[ ]*[^ ])/gim, function( c ) { return c.toUpperCase(); } );
+	text = text.replace( /(^==[ ]*[^ ])/gim, function( c ) { return c.toUpperCase(); } );
+	e.value = text;
+}
+JS;
+	$toolbar .= '<div style="float: left;"><input id="fixcaps_button" type="image" src="' .
+		$wgExtensionAssetsPath . '/FixCaps/fixcaps.png" accesskey="o" onclick="javascript:fixcaps(document.editform.wpTextbox1)" /></div>' .
+		"\n";
+
+	return true;
 }
