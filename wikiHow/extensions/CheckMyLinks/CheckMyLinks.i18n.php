a25eddbb4eadc5e69ea0451bc176e802c6040ad6
Jack Phoenix <jack@countervandalism.net>
2012-03-18 13:53:31 +0000

    CheckMyLinks: *three-file approach *proper i18n *code comments *check login state early on and if the user isn't logged in, show the error message and bail out
    To-do/FIXME: main setup file needs to be renamed to CamelCase (this is relatively difficult on Windows due to Windows' case-insensitive filesystem)

---
+++ wikiHow/extensions/CheckMyLinks/CheckMyLinks.i18n.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Internationalization file for the CheckMyLinks extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English */
+$messages['en'] = array(
+	'checkmylinks' => 'Check My Links',
+	'checkmylinks-error' => 'Your account does not have any links asscociated with it. Please see [[Create Custom Navigation Links on Your Sidebar on wikiHow]].',
+	'checkmylinks-not-logged-in' => 'You have to be logged in to use this feature.',
+	'checkmylinks-size-bad' => 'Your links are $1 bytes long. These are too long, please shorten them to allow them to less than 3,000 to allow them to work. ',
+	'checkmylinks-size-good' => 'Your links are $1 bytes long. These are OK!',
+	'checkmylinks-summary' => 'This is a simple page that will tell you whether or not your "[[Create Custom Navigation Links on Your Sidebar on wikiHow|My links]]" are too long.
+Please [[Special:UserLogout|log out]] and [[Special:UserLogin|log back in]] again after making any changes to see your most recent links.',
+);
