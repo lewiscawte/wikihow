1dc63b87c83c1502933398ffe8512dceffaca2f9
Jack Phoenix <jack@countervandalism.net>
2012-03-18 12:52:47 +0000

    FBNuke: *add new fbnuke permission, checks for it and give it to bureaucrats by default *add i18n *remove some DB switch calls -- you should be using $wgSharedDB & $wgSharedTables properly, i.e. have user and facebook_connect tables in $wgSharedTables instead of changing databases all the time *add extension credits *add Finnish (fi) translation

--- wikiHow/extensions/FBNuke/FBNuke.body.php
+++ wikiHow/extensions/FBNuke/FBNuke.body.php
@@ -1,68 +1,105 @@
-<?
-class FBNuke extends UnlistedSpecialPage {
+<?php
 
-	function __construct() { 
-		UnlistedSpecialPage::UnlistedSpecialPage( 'FBNuke' );
+class FBNuke extends SpecialPage {
+
+	/**
+	 * Constructor -- set up the new special page
+	 */
+	public function __construct() {
+		parent::__construct( 'FBNuke', 'fbnuke' );
 	}
-	
-	function execute($par) {
+
+	/**
+	 * Show the special page
+	 *
+	 * @param $par Mixed: parameter passed to the page or null
+	 */
+	public function execute( $par ) {
 		global $wgOut, $wgRequest, $wgUser;
-		if ($wgUser->getName() != 'Jordansmall') {
-			$wgOut->showErrorPage( 'nosuchspecialpage', 'prefsnologintext' );
+
+		// Check permissions
+		if ( !$wgUser->isAllowed( 'fbnuke' ) ) {
+			$this->displayRestrictionError();
 			return;
 		}
 
-		$wgOut->addHTML("<h1>Remove Facebook Account</h1><br /><br />");
+		// Show a message if the database is in read-only mode
+		if ( wfReadOnly() ) {
+			$wgOut->readOnlyPage();
+			return;
+		}
 
-		$userName = $wgRequest->getVal('uname');
-		$removeWikiHowAcct = $wgRequest->getVal('whremove', '') == 'on' ? true : false;
-		if ($userName) {
-			$this->removeAccount($userName, $removeWikiHowAcct);
+		// If user is blocked, s/he doesn't need to access this page
+		if ( $wgUser->isBlocked() ) {
+			$wgOut->blockedPage();
+			return;
+		}
+
+		// Set headers, such as the page title, robot policy, etc.
+		$this->setHeaders();
+
+		$userName = $wgRequest->getVal( 'uname', $par );
+		$removeWikiAccount = $wgRequest->getVal( 'whremove', '' ) == 'on' ? true : false;
+		if ( $userName ) {
+			$this->removeAccount( $userName, $removeWikiAccount );
 		}
 		$this->showForm();
-		$dbw = wfGetDB(DB_MASTER);	
-		$dbw->selectDB(WH_DATABASE_NAME);
 	}
 
 	function showForm() {
 		global $wgOut;
-		
-		$html = "<form action='/Special:FBNuke' method='get'>";
-		$html .= "<p><label for='uname'>Username: </label>";
-		$html .= "<input type='text' name='uname' /></p>";
-		$html .= "<p><input type='checkbox' name='whremove' /> <label for='whremove'>Remove wikiHow account in addition to FB Account</label></p>";
-		$html .= "<input type='submit' name='remove' value='remove'/>";
-		$html .= "</form>";
-		$wgOut->addHTML($html);
+
+		$html = '<form action="' . $this->getTitle()->getFullURL() . '" method="get">';
+		$html .= '<p><label for="uname">' . wfMsg( 'fbnuke-username' ) . ' </label>';
+		$html .= '<input type="text" name="uname" /></p>';
+		$html .= '<p><input type="checkbox" name="whremove" /> <label for="whremove">' .
+			wfMsgExt( 'fbnuke-remove-wiki-account', 'parsemag' ) . '</label></p>';
+		$html .= '<input type="submit" name="remove" value="' .
+			wfMsg( 'fbnuke-submit-button' ) . '" />';
+		$html .= '</form>';
+		$wgOut->addHTML( $html );
 	}
 
-	function removeAccount($userName, $removeWikiHowAcct = false) {
-		global $wgSharedDB, $wgOut, $wgUser;
+	function removeAccount( $userName, $removeWikiAccount = false ) {
+		global $wgOut, $wgUser;
 
-		$dbw = wfGetDB(DB_MASTER);	
-		$dbw->selectDB($wgSharedDB);
+		$dbw = wfGetDB( DB_MASTER );
 
-		$userName = $dbw->strEncode($userName);
-		if(strtolower($wgUser->getName()) == strtolower($userName)) {
-			$wgOut->addHTML("<h4>Error: Can't remove your own username</h4><br /><br />");
+		$userName = $dbw->strencode( $userName );
+		if( strtolower( $wgUser->getName() ) == strtolower( $userName ) ) {
+			$wgOut->addHTML(
+				'<h4>' . wfMsg( 'fbnuke-error-cant-remove-self' ) .
+				'</h4><br /><br />'
+			);
 			return;
 		}
 
-		$userId = $dbw->selectField('user', array('user_id'), array('user_name'=>$userName));
-		if (!$userId) {
-			$wgOut->addHTML("<h4>$userName not found in the database</h4><br /><br />");
+		$userId = $dbw->selectField(
+			'user',
+			array( 'user_id' ),
+			array( 'user_name' => $userName ),
+			__METHOD__
+		);
+		if ( !$userId ) {
+			$wgOut->addHTML(
+				'<h4>' . wfMsg( 'fbnuke-user-not-found', $userName ) .
+				'</h4><br /><br />'
+			);
 			return;
 		}
 
-		$sql = "DELETE FROM facebook_connect where wh_user = $userId";
-		$dbw->query($sql);
+		$dbw->delete(
+			'facebook_connect',
+			array( 'wh_user' => $userId ),
+			__METHOD__
+		);
 
-		if ($removeWikiHowAcct) {
-			$sql = "DELETE FROM user where user_id = $userId";
-			$dbw->query($sql);
+		if ( $removeWikiAccount ) {
+			$dbw->delete( 'user', array( 'user_id' => $userId ), __METHOD__ );
 		}
 
-		$wgOut->addHTML("<h4>$userName removed from the database</h4><br /><br />");
-
+		$wgOut->addHTML(
+			'<h4>' . wfMsg( 'fbnuke-success', $userName ) . '</h4><br /><br />'
+		);
 	}
 }
