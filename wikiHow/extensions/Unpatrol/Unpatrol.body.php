362a790d547c0cc64480ce7b9e151718fa5ebff7
Jack Phoenix <jack@countervandalism.net>
2012-03-18 12:02:14 +0000

    Unpatrol: *indented code with tabs (instead of spaces) *introduced proper i18n *tweaked extension credits *introduced new 'unpatrol' user right and gave it to bureaucrats by default instead of checking for group membership in Unpatrol::execute() *made the special page listed instead of unlisted *added standard checks for user's block status and database read-only mode in Unpatrol::execute()

--- wikiHow/extensions/Unpatrol/Unpatrol.body.php
+++ wikiHow/extensions/Unpatrol/Unpatrol.body.php
@@ -1,85 +1,141 @@
-<?
-class Unpatrol extends UnlistedSpecialPage {
+<?php
 
-    function __construct() {
-        UnlistedSpecialPage::UnlistedSpecialPage( 'Unpatrol' );
-    }
+class Unpatrol extends SpecialPage {
 
-		function padVar ($name) {
-			global $wgRequest;
-			$val = $wgRequest->getVal($name);
-			if ($val && strlen($val) < 2)
-				$val = "0" . $val;
-			return $val;
+	/**
+	 * Constructor -- set up the new special page
+	 */
+	public function __construct() {
+		parent::__construct( 'Unpatrol', 'unpatrol' );
+	}
+
+	function padVar( $name ) {
+		global $wgRequest;
+		$val = $wgRequest->getVal( $name );
+		if ( $val && strlen( $val ) < 2 ) {
+			$val = '0' . $val;
+		}
+		return $val;
+	}
+
+	/**
+	 * Show the special page
+	 *
+	 * @param $par Mixed: parameter passed to the page or null
+	 */
+	public function execute( $par ) {
+		global $wgOut, $wgRequest, $wgUser;
+
+		// Check permissions
+		if ( !$wgUser->isAllowed( 'unpatrol' ) ) {
+			$this->displayRestrictionError();
+			return;
 		}
 
-    function execute ($par) {
-		global $wgOut, $wgRequest, $wgUser, $wgLang;
-
-		if ( !in_array( 'staff', $wgUser->getGroups() ) ) {
-         	$wgOut->errorpage( 'nosuchspecialpage', 'nospecialpagetext' );
-         	return;
+		// Show a message if the database is in read-only mode
+		if ( wfReadOnly() ) {
+			$wgOut->readOnlyPage();
+			return;
 		}
 
-		$wgOut->addHTML("
-				<form action='/Special:Unpatrol' method='POST'>
-					Username: <input type='text' name='username' value='{$wgRequest->getVal('username', '')}'/> <br/><br/>
-					Start date: Year: " . date("Y") . " Month: <input type='text' name='month_1' size='2' value='" . date("m") . "'/>
-							Day: <input type='text' name='day_1' size='2' value='" . date("d") . "'>
-							Hour (GMT): <input type='text' name='hour_1' size='2' value='00'> <br/><br/>
-					End date (optional): Year: " . date("Y") . " <input type='text' name='month_2' size='2'>
-                            Day: <input type='text' name='day_2' size='2'>
-                            Hour (GMT): <input type='text' name='hour_2' size='2' value='00'> <br/><br/>
-					<input type='submit'/>
-				</form>	");
-		if ($wgRequest->wasPosted()) {
-    		$user   = $wgRequest->getVal('username');
-    	
-			$start  = date("Y") . $this->padVar('month_1') . $this->padVar('day_1') . $this->padVar('hour_1') . "0000";
-			$end =  $wgRequest->getVal('month_2') ? 
-						date("Y") . $this->padVar('month_2') . $this->padVar('day_2') . $this->padVar('hour_2') . "0000" : null;
-		
-		    $cutoff = wfTimestamp(TS_MW, $start);
-		    $cutoff2 = null;
-		    if (!$end) {
-		        $wgOut->addHTML("reverting changes by $user since {$cutoff}<br/>");
-		    } else {
-		        $cutoff2 = wfTimestamp(TS_MW, $end);
-		        $wgOut->addHTML("reverting changes by $user between {$cutoff} and {$cutoff2} <br/>");
-		    }
-		
-		    $user = User::newFromName($user);
+		// If user is blocked, s/he doesn't need to access this page
+		if ( $wgUser->isBlocked() ) {
+			$wgOut->blockedPage();
+			return;
+		}
 
-			if ($user->getID() == 0) {
-				$wgOut->addHTML("<b>WHoa! There is no user with this name {$wgRequest->getVal('username', '')}, bailing.</b>");
+		$username = $wgRequest->getVal( 'username', $par );
+
+		$wgOut->addHTML(
+			'<form action="' . $this->getTitle()->getFullURL() . '" method="post">' .
+			wfMsg( 'unpatrol-username' ) . ' <input type="text" name="username" value="' . $username . '" / > <br /><br />' .
+			wfMsg( 'unpatrol-startdate' ) . wfMsg( 'word-separator' ) . wfMsg( 'unpatrol-year' ) .
+			date( 'Y' ) . ' ' . wfMsg( 'unpatrol-month' ) . ' <input type="text" name="month_1" size="2" value="' . date( 'm' ) . '" />' .
+			wfMsg( 'unpatrol-day' ) . ' <input type="text" name="day_1" size="2" value="' . date( 'd' ) . '" />' .
+			wfMsg( 'unpatrol-hour' ) . ' <input type="text" name="hour_1" size="2" value="00" /> <br /><br />' .
+			wfMsg( 'unpatrol-enddate' ) . wfMsg( 'word-separator' ) . wfMsg( 'unpatrol-year' ) .
+			date( 'Y' ) . ' <input type="text" name="month_2" size="2" />' .
+			wfMsg( 'unpatrol-day' ) . ' <input type="text" name="day_2" size="2" />' .
+			wfMsg( 'unpatrol-hour' ) . ' <input type="text" name="hour_2" size="2" /> <br /><br />
+			<input type="submit" />
+			</form>'
+		);
+
+		if ( $wgRequest->wasPosted() ) {
+			$user = $wgRequest->getVal( 'username' );
+
+			$start = date( 'Y' ) . $this->padVar( 'month_1' ) .
+				$this->padVar( 'day_1' ) . $this->padVar( 'hour_1' ) . '0000';
+
+			$end = null;
+			if ( $wgRequest->getVal( 'month_2' ) ) {
+				$end = date( 'Y' ) . $this->padVar( 'month_1' ) .
+					$this->padVar( 'day_2' ) . $this->padVar( 'hour_2' ) . '0000';
+			}
+
+			$cutoff = wfTimestamp( TS_MW, $start );
+			$cutoff2 = null;
+			if ( !$end ) {
+				$wgOut->addHTML( wfMsg( 'unpatrol-reverting', $user, $cutoff ) . '<br />' );
+			} else {
+				$cutoff2 = wfTimestamp( TS_MW, $end );
+				$wgOut->addHTML( wfMsg( 'unpatrol-reverting-between', $user, $cutoff, $cutoff2 ) . '<br />' );
+			}
+
+			$user = User::newFromName( $user );
+
+			if ( $user->getID() == 0 ) {
+				$wgOut->addHTML( wfMsg( 'unpatrol-nouser', $wgRequest->getVal( 'username', '' ) ) );
 				return;
 			}
-		
-		    $dbw = wfGetDB(DB_MASTER);
-		    $options = array('log_user'=>$user->getID(), 'log_type'=>'patrol', "log_timestamp > '{$cutoff}'");
-		    if ($cutoff2)
-		        $options[] = "log_timestamp < '{$cutoff2}'";
-		
-		    $res = $dbw->select('logging', array('log_title', 'log_params'), $options);
-		
-		
-		    $oldids = array();
-		    while ($row = $dbw->fetchObject($res)) {
-		        #echo "{$row->log_title}\t". str_replace("\n", " ", $row->log_params) . "\n";
-		        $oldids[]= preg_replace("@\n.*@", "", $row->log_params);
-		    }
-			if (sizeof($oldids) > 0) {		
-		    	$count = $dbw->query("UPDATE recentchanges set rc_patrolled=0 where rc_this_oldid IN (" . implode(", ", $oldids) . ");");
-				wfRunHooks('Unpatrol', array(&$oldids));
-    			$wgOut->addHTML( "Unpatrolled " . sizeof($oldids) . " patrols by {$user->getName()}\n");
+
+			$dbw = wfGetDB( DB_MASTER );
+			$options = array(
+				'log_user' => $user->getID(),
+				'log_type' => 'patrol',
+				"log_timestamp > '{$cutoff}'"
+			);
+
+			if ( $cutoff2 ) {
+				$options[] = "log_timestamp < '{$cutoff2}'";
+			}
+
+			$res = $dbw->select(
+				'logging',
+				array( 'log_title', 'log_params' ),
+				$options,
+				__METHOD__
+			);
+
+			$oldIds = array();
+			foreach ( $res as $row ) {
+				$oldIds[] = preg_replace( "@\n.*@", '', $row->log_params );
+			}
+
+			if ( sizeof( $oldIds ) > 0 ) {
+				$count = $dbw->update(
+					'recentchanges',
+					array( 'rc_patrolled' => 0 ),
+					array( 'rc_this_oldid IN (' . implode( ', ', $oldIds ) . ')' ),
+					__METHOD__
+				);
+				wfRunHooks( 'Unpatrol', array( &$oldIds ) );
+				$wgOut->addHTML( wfMsgExt( 'unpatrol-done', 'parsemag', sizeof( $oldIds ), $user->getName() ) . "\n" );
 
 				$log = new LogPage( 'unpatrol', false );
-				$msg = wfMsgHtml("unpatrol_log", sizeof($oldids), "[[User:" . $user->getName() . "]]", $wgLang->date($cutoff), $cutoff2==null?$wgLang->date(wfTimestampNow()):$wgLang->date($cutoff2));
-				$log->addEntry('unpatrol', $this->getTitle(), $msg);
+				$msg = wfMsgHtml(
+					'unpatrol-log-entry',
+					sizeof( $oldIds ),
+					'[[User:' . $user->getName() . ']]',
+					$wgLang->date( $cutoff ),
+					$cutoff2 == null ? $wgLang->date( wfTimestampNow() ) : $wgLang->date( $cutoff2 )
+				);
+				$log->addEntry( 'unpatrol', $this->getTitle(), $msg );
 			} else {
-    			$wgOut->addHTML( "There were no patrolled edits to undo for this time frame.<br/>");
-			}	
-		}		
-		return;	
+				$wgOut->addHTML( wfMsg( 'unpatrol-noedits' ) . '<br />' );
+			}
+		}
+
+		return;
 	}
 }
--- wikiHow/extensions/Unpatrol/Unpatrol.php
+++ wikiHow/extensions/Unpatrol/Unpatrol.php
@@ -1,31 +1,37 @@
 <?php
-if ( ! defined( 'MEDIAWIKI' ) )
-	die();
-    
-/**#@+
- * An extension that allows users to rate articles. 
- * 
- * @package MediaWiki
- * @subpackage Extensions
+/**
+ * An extension that allows privileged users to unpatrol bad patrols.
  *
- * @link http://www.wikihow.com/WikiHow:Unpatrol-Extension Documentation
- *
- *
+ * @file
+ * @ingroup Extensions
+ * @version 1.0
  * @author Travis Derouin <travis@wikihow.com>
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die();
+}
 
+// Extension credits that will show up on Special:Version
 $wgExtensionCredits['specialpage'][] = array(
-    'name' => 'Unpatrol',
-    'author' => 'Travis <travis@wikihow.com>',
-    'description' => 'Unpatrol bad patrols',
+	'name' => 'Unpatrol',
+	'version' => '1.0',
+	'author' => 'Travis Derouin',
+	'description' => 'Allows privileged users to [[Special:Unpatrol|unpatrol]] bad patrols',
 );
 
+// Set up the new special page
+$dir = dirname( __FILE__ ) . '/';
+$wgExtensionMessagsFiles['Unpatrol'] = $dir . 'Unpatrol.i18n.php';
+$wgAutoloadClasses['Unpatrol'] = $dir . 'Unpatrol.body.php';
 $wgSpecialPages['Unpatrol'] = 'Unpatrol';
-$wgAutoloadClasses['Unpatrol'] = dirname( __FILE__ ) . '/Unpatrol.body.php';
 
+// New user right, required to use Special:Unpatrol
+$wgAvailableRights[] = 'unpatrol';
+$wgGroupPermissions['bureaucrat']['unpatrol'] = true;
+
+// Logging
 $wgLogTypes[] = 'unpatrol';
 $wgLogNames['unpatrol'] = 'unpatrol';
 $wgLogHeaders['unpatrol'] = 'unpatrol';
-
