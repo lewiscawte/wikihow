b48e48ed0de0b8e40b691d3f784b7e8f5674f8ad
Jack Phoenix <jack@countervandalism.net>
2012-03-18 15:03:01 +0000

    AdminRemoveAvatar: *moved JS into its own file and loaded it with ResourceLoader [untested] *introduced new 'adminremoveavatar' user right and checked for it in AdminRemoveAvatar::execute() instead of checking for group membership; named it so because SocialProfile already has the 'removeavatar' user right; gave the new user right to bureaucrat group by default *added i18n *made the special page a listed one, no reason for this to be an unlisted one *swapped the error suppression operator, @, to wfSuppressWarnings() and wfRestoreWarnings(), as per MediaWiki's coding conventions *added standard checks for DB read-only mode & user's block status to AdminRemoveAvatar::execute() *coding style tweaks everywhere

--- wikiHow/extensions/AdminRemoveAvatar/AdminRemoveAvatar.body.php
+++ wikiHow/extensions/AdminRemoveAvatar/AdminRemoveAvatar.body.php
@@ -1,41 +1,50 @@
-<?
+<?php
 
-if (!defined('MEDIAWIKI')) die();
+class AdminRemoveAvatar extends SpecialPage {
 
-class AdminRemoveAvatar extends UnlistedSpecialPage {
-
-	function __construct() {
-		UnlistedSpecialPage::UnlistedSpecialPage('AdminRemoveAvatar');
+	/**
+	 * Constructor -- set up the new special page
+	 */
+	public function __construct() {
+		parent::__construct( 'AdminRemoveAvatar', 'adminremoveavatar' );
 	}
 
 	/**
 	 * Pull a user account (by username) and remove the avatar file associated.
 	 *
-	 * @param $username string, the username
-	 * @return true or false (true iff action was successful)
+	 * @param $username String: the username
+	 * @return Boolean: true if action was successful
 	 */
-	function removeAvatar($username) {
+	function removeAvatar( $username ) {
 		global $IP;
-		$user = User::newFromName($username);
+
+		$user = User::newFromName( $username );
 		$userID = $user->getID();
-		if ($userID > 0) {
+
+		if ( $userID > 0 ) {
 			// TODO1: log this action somewhere, along with the user who did it
 			// TODO2: Purge /User:$username page from Varnish
 
-			$imgDir = Avatar::getAvatarOutPath("$userID.jpg");
+			$imgDir = Avatar::getAvatarOutPath( "$userID.jpg" );
 			$path = "$IP$imgDir$userID.jpg";
 			$ret = false;
-			if (file_exists($path)) {
-				$ret = @unlink($path);
+			if ( file_exists( $path ) ) {
+				wfSuppressWarnings();
+				$ret = unlink( $path );
+				wfRestoreWarnings();
 			}
 
 			$ret = true;
-			// Hack: Pick some arbitrary number of files to scan through. Let's say 50
-			for ($i = 0; $i < 50; $i++) {
+			// Hack: Pick some arbitrary number of files to scan through.
+			// Let's say 50
+			for ( $i = 0; $i < 50; $i++ ) {
 				$imgDir = Avatar::getAvatarOutPath("$userID-$i.jpg");
 				$imgPath = "$IP$imgDir$userID-$i.jpg";
-				if (file_exists($imgPath)) {
-					if (!@unlink($file)) {
+				if ( file_exists( $imgPath ) ) {
+					wfSuppressWarnings();
+					$unlinkedFile = unlink( $file );
+					wfRestoreWarnings();
+					if ( !$unlinkedFile ) {
 						$ret = false;
 						break;
 					}
@@ -49,86 +58,88 @@ class AdminRemoveAvatar extends UnlistedSpecialPage {
 	}
 
 	/**
-	 * Execute special page, but only for staff group members
+	 * Show the special page
+	 *
+	 * @param $par Mixed: parameter passed to the special page or null
 	 */
-	function execute() {
-		global $wgRequest, $wgOut, $wgUser, $wgLang, $wgSquidMaxage;
+	public function execute( $par ) {
+		global $wgRequest, $wgOut, $wgUser, $wgSquidMaxage;
 
-		$userGroups = $wgUser->getGroups();
-		if ($wgUser->isBlocked() || !in_array('sysop', $userGroups)) {
-			$wgOut->setRobotpolicy('noindex,nofollow');
-			$wgOut->errorpage('nosuchspecialpage', 'nospecialpagetext');
+		// Check permissions
+		if ( !$wgUser->isAllowed( 'adminremoveavatar' ) ) {
+			$this->displayRestrictionError();
 			return;
 		}
 
-		if ($wgRequest->wasPosted()) {
-			$username = $wgRequest->getVal('username', '');
-			$wgOut->setArticleBodyOnly(true);
-			$success = $this->removeAvatar($username);
-			if ($success) {
-				$url = 'http://www.wikihow.com/User:' . preg_replace('@ @', '-', $username);
-				$cacheHours = round(1.0 * $wgSquidMaxage / (60 * 60), 1);
-				$tmpl = <<<EOHTML
-<p>Avatar for '$username' removed from user page.  This change will be visible to non-cookied users within $cacheHours hours and will be visible to cookied users immediately.</p>
-<p>See results: <a href='$url'>$url</a></p>
-EOHTML;
-				$result = array('result' => $tmpl);
+		// Show a message if the database is in read-only mode
+		if ( wfReadOnly() ) {
+			$wgOut->readOnlyPage();
+			return;
+		}
+
+		// If the user is blocked, they don't need to access this page
+		if ( $wgUser->isBlocked() ) {
+			$wgOut->blockedPage();
+			return;
+		}
+
+		// The JS file POSTs into this very page
+		if ( $wgRequest->wasPosted() ) {
+			$username = $wgRequest->getVal( 'username', '' );
+
+			// So that we don't get a special page inside a special page :P
+			// We're not interested in the skin, we just need the content
+			$wgOut->setArticleBodyOnly( true );
+
+			$success = $this->removeAvatar( $username );
+
+			if ( $success ) {
+				$cacheHours = round( 1.0 * $wgSquidMaxage / ( 60 * 60 ), 1 );
+				$tmpl = wfMessage( 'adminremoveavatar-removed', $username, $cacheHours )->parseAsBlock();
+				$result = array( 'result' => $tmpl );
 
 				// Log the removal
-				$log = new LogPage('avatarrm', false); // false - dont show in recentchanges
+				$log = new LogPage( 'avatarrm', false ); // false - don't show in recentchanges
 				$params = array();
-				$log->addEntry('', Title::newFromText('User:' . $username), 'admin "' . $wgUser->getName() . '" removed avatar for username: ' . $username, $params);
-
+				$log->addEntry(
+					'',
+					Title::newFromText( $username, NS_USER ),
+					wfMessage( 'adminremoveavatar-log-entry', $wgUser->getName(), $username )->inContentLanguage()->plain(),
+					$params
+				);
 			} else {
-				$result = array('result' => "error: either user '$username' not found or '$username' didn't have an avatar");
+				$result = array(
+					'result' => wfMsg( 'adminremoveavatar-error', $username )
+				);
 			}
-			print json_encode($result);
+			echo json_encode( $result );
 			return;
 		}
 
-		$wgOut->setHTMLTitle('Admin - Remove Avatar - wikiHow');
+		// Set headers, such as the page title, robot policy, etc.
+		$this->setHeaders();
 
-$tmpl = <<<EOHTML
-<form method="post" action="/Special:AdminRemoveAvatar">
-<p>The only images you should remove are those with nudity, obscenity, violence, or expressions of hate - everything else is fair game</p>
-<br/>
-<h4>Enter username of avatar to remove</h4>
-<br/>
+		$rules = wfMsg( 'adminremoveavatar-rules' );
+		$enter = wfMsg( 'adminremoveavatar-enter-username' );
+		$reset = wfMsg( 'adminremoveavatar-reset' );
+		$postURL = $this->getTitle()->getFullURL();
+		$tmpl = <<<EOHTML
+<form method="post" action="$postURL">
+<p>$rules</p>
+<br />
+<h4>$enter</h4>
+<br />
 <input id="admin-username" type="text" size="40" />
-<button id="admin-go" disabled="disabled">reset</button><br/>
+<button id="admin-go" disabled="disabled">$reset</button><br />
 <br/>
 <div id="admin-result"></div>
 </form>
-
-<script>
-(function($) {
-	$(document).ready(function() {
-		$('#admin-go')
-			.attr('disabled', '')
-			.click(function () {
-				$('#admin-result').html('loading ...');
-				$.post('/Special:AdminRemoveAvatar',
-					{ 'username': $('#admin-username').val() },
-					function(data) {
-						$('#admin-result').html(data['result']);
-						$('#admin-username').focus();
-					},
-					'json');
-				return false;
-			});
-		$('#admin-username')
-			.focus()
-			.keypress(function (evt) {
-				if (evt.which == 13) { // if user hits 'enter' key
-					$('#admin-go').click();
-					return false;
-				}
-			});
-	});
-})(jQuery);
-</script>
 EOHTML;
 
-		$wgOut->addHTML($tmpl);
+		// Add JS
+		$wgOut->addModules( 'ext.adminRemoveAvatar' );
+
+		// Output the form
+		$wgOut->addHTML( $tmpl );
 	}
 }
--- wikiHow/extensions/AdminRemoveAvatar/AdminRemoveAvatar.php
+++ wikiHow/extensions/AdminRemoveAvatar/AdminRemoveAvatar.php
@@ -1,17 +1,45 @@
 <?php
+/**
+ * AdminRemoveAvatar extension -- allows privileged users to remove a user's
+ * avatar file
+ *
+ * @file
+ * @ingroup Extensions
+ * @version 1.0
+ * @author Reuben Smith <reuben@wikihow.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die();
+}
 
-if ( !defined('MEDIAWIKI') ) die();
-    
+// Extension credits that will show up on Special:Version
 $wgExtensionCredits['specialpage'][] = array(
 	'name' => 'AdminRemoveAvatar',
+	'version' => '1.0',
 	'author' => 'Reuben Smith',
-	'description' => 'Tool for support personnel to remove a user\'s avatar file',
+	'description' => '[[Special:AdminRemoveAvatar|Tool]] for support personnel to remove a user\'s avatar file',
 );
 
+// Set up the new special page
+$dir = dirname( __FILE__ ) . '/';
+$wgExtensionMessagesFiles['AdminRemoveAvatar'] = $dir . 'AdminRemoveAvatar.i18n.php';
+$wgAutoloadClasses['AdminRemoveAvatar'] = $dir . 'AdminRemoveAvatar.body.php';
 $wgSpecialPages['AdminRemoveAvatar'] = 'AdminRemoveAvatar';
-$wgAutoloadClasses['AdminRemoveAvatar'] = dirname( __FILE__ ) . '/AdminRemoveAvatar.body.php';
 
+// Register the JS with ResourceLoader
+$wgResourceModules['ext.adminRemoveAvatar'] = array(
+	'scripts' => 'AdminRemoveAvatar.js',
+	'messages' => array( 'adminremoveavatar-loading' ),
+ 	'localBasePath' => dirname( __FILE__ ),
+	'remoteExtPath' => 'AdminRemoveAvatar'
+);
+
+// New user right, required to use the special page
+$wgAvailableRights[] = 'adminremoveavatar';
+$wgGroupPermissions['bureaucrat']['adminremoveavatar'] = true;
+
+// Set up logging
 $wgLogTypes[]             = 'avatarrm';
 $wgLogNames['avatarrm']   = 'avatarrm';
 $wgLogHeaders['avatarrm'] = 'avatarrmtext';
-
