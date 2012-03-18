d071c47a4561d852ffe15725566cb169a22567b9
Jack Phoenix <jack@countervandalism.net>
2012-03-18 09:51:14 +0000

    ImageCaptions: *add extension credits *coding style *international compatibility -- check for the "File" namespace in the local language, too, in addition to the English "Image". Regex is totally untested, but based on http://www.mediawiki.org/wiki/Special:Code/MediaWiki/89731

--- wikiHow/extensions/ImageCaptions/ImageCaptions.php
+++ wikiHow/extensions/ImageCaptions/ImageCaptions.php
@@ -1,20 +1,45 @@
-<?
-$wgHooks['ParserBeforeStrip'][] = array('wfAddCaptionsToImageSteps');
+<?php
+/**
+ * ImageCaptions extension -- adds a caption to images in steps that don't have
+ * them.
+ *
+ * @file
+ * @ingroup Extensions
+ * @version 1.0
+ * @author Travis Derouin <travis@wikihow.com>
+ * @author Jack Phoenix <jack@countervandalism.net>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @link http://www.mediawiki.org/wiki/Extension:ImageCaptions Documentation
+ */
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die();
+}
 
-/****
- *
- *
+// Extension credits that will show up on Special:Version
+$wgExtensionCredits['other'][] = array(
+	'name' => 'ImageCaptions',
+	'version' => '1.0',
+	'author' => array( 'Travis Derouin', 'Jack Phoenix' ),
+	'description' => "Adds a caption to images in steps that don't have them",
+	'url' => 'http://www.mediawiki.org/wiki/Extension:ImageCaptions',
+);
+
+$wgHooks['ParserBeforeStrip'][] = 'wfAddCaptionsToImageSteps';
+
+/**
  * Adds a caption to images in steps that don't have them.
- *
- */ 
-function wfAddCaptionsToImageSteps(&$parser, &$text, &$stripstate) {
+ */
+function wfAddCaptionsToImageSteps( &$parser, &$text, &$stripstate ) {
+	global $wgContLang;
+
+	$fileNamespaceName = $wgContLang->getNsText( NS_FILE );
 
 	// only do this on main namespace articles that have steps
 	$title = $parser->mTitle;
-	if (!$title || $title->getNamespace() != NS_MAIN) {
+	if ( !$title || $title->getNamespace() != NS_MAIN ) {
 		return true;
 	}
-	if (!preg_match("@==[ ]*" . wfMsg('steps') . "@", $text)) {
+	if ( !preg_match( "@==[ ]*" . wfMsg( 'steps' ) . '@', $text ) ) {
 		return true;
 	}
 
@@ -23,88 +48,92 @@ function wfAddCaptionsToImageSteps(&$parser, &$text, &$stripstate) {
 	// we have to do it ourselves
 	$index = 0;
 	$steps = null;
-	//$sections = preg_split("@^(==[^=]*==)@m", $text, 0, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
-	$sections = preg_split("@^(==[^=]+==)@m", $text, 0, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
-	foreach ($sections as $section) {
+	//$sections = preg_split( "@^(==[^=]*==)@m", $text, 0, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY );
+	$sections = preg_split( "@^(==[^=]+==)@m", $text, 0, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY );
+	foreach ( $sections as $section ) {
 		$index++;
-		if (preg_match("@^==[ ]*" . wfMsg('steps') . "@", $section)) {
-			$steps = preg_split("@\n@m", $sections[$index]);
+		if ( preg_match( "@^==[ ]*" . wfMsg( 'steps' ) . '@', $section ) ) {
+			$steps = preg_split( "@\n@m", $sections[$index] );
 			break;
 		}
 	}
-	
+
 	// barf!
-	if (!$steps ) {
+	if ( !$steps ) {
 		return true;
 	}
 
 	// iterate over the steps, find the images for each one
-	$newsteps = array();
+	$newSteps = array();
 	$changed = false;
-	foreach ($steps as $s) {
+	foreach ( $steps as $s ) {
 		// pretty the step up, remove images, etc.
-		$p  = preg_replace("@\[\[Image:[^\]]*\]\]@", "", $s);
-		preg_match("@^#[^\.]*@", $p, $matches);
-		$f = trim(preg_replace("@^#@", "", $matches[0]));
-		$f = preg_replace("@''[']?@", "", $f) . ".";
-		preg_match_all("@\[\[[^\]]*\]\]@", $f, $matches);
-		foreach ($matches[0] as $m) {
-			if (preg_match("@\|@", $m)) {
-				$x = preg_replace("@.*\|@", "", $m);
-				$f = str_replace($m, $x, $f);
+		$p  = preg_replace( "@\[\[(:?Image|$fileNamespaceName):[^\]]*\]\]@", '', $s );
+		preg_match( "@^#[^\.]*@", $p, $matches );
+		$f = trim( preg_replace( "@^#@", '', $matches[0] ) );
+		$f = preg_replace( "@''[']?@", '', $f ) . '.';
+		preg_match_all( "@\[\[[^\]]*\]\]@", $f, $matches );
+		foreach ( $matches[0] as $m ) {
+			if ( preg_match( "@\|@", $m ) ) {
+				$x = preg_replace( "@.*\|@", '', $m );
+				$f = str_replace( $m, $x, $f );
 			}
 		}
-		$f = preg_replace("@\[|\]@", "", $f);
-		$f = preg_replace("@^[^A-Za-z0-9]*@", "", $f); 
-		preg_match("@\[\[Image:[^\]]*\]\]@", $s, $images); 
-		if (sizeof($images) > 0) {
+		$f = preg_replace( "@\[|\]@", '', $f );
+		$f = preg_replace( "@^[^A-Za-z0-9]*@", '', $f );
+		preg_match( "@\[\[(:?Image|$fileNamespaceName):[^\]]*\]\]@", $s, $images );
+		if ( sizeof( $images ) > 0 ) {
 			// just put a caption in the first image
-			$parts = preg_split("@\|@", preg_replace("@\[|\]@", "", $images[0]));
-			if (sizeof($parts) == 1) {
+			$parts = preg_split( "@\|@", preg_replace( "@\[|\]@", '', $images[0] ) );
+			if ( sizeof( $parts ) == 1 ) {
 				// boundary case here it's just [[Image:hidyho.jpg]]
-				$img = "[[" . $parts[0] . "|". $f . "]]";
-			} else { 
+				$img = '[[' . $parts[0] . '|' . $f . ']]';
+			} else {
 				// otherwise it has params
-				$last = $parts[sizeof($parts)-1];
+				$last = $parts[sizeof( $parts ) - 1];
 
 				// ignore the "description" caption and empty descriptions
-				if (strtolower($last) == "description" || trim($last) == "") {
-					array_pop($parts);
-					$last = $parts[sizeof($parts)-1];
+				if ( strtolower( $last ) == 'description' || trim( $last ) == '' ) {
+					array_pop( $parts );
+					$last = $parts[sizeof( $parts ) - 1];
 				}
 
 				$caption = false;
-				foreach ($parts as $p) {
-					if (!preg_match("@px$@", $p) 
-						&& !preg_match("@^Image:@", $p) 
-						&& !in_array($p, array("thumb", "border", "right", "left"))) {
+				foreach ( $parts as $p ) {
+					if (
+						!preg_match( "@px$@", $p ) &&
+						!preg_match( "@^(:?Image|$fileNamespaceName):@", $p )
+						&& !in_array( $p, array( 'thumb', 'border', 'right', 'left' ) )
+					)
+					{
 						$caption = true;
 					}
 				}
 				// do we have a caption?
-				if (!$caption) {
+				if ( !$caption ) {
 					// if the thumb param isn't there, the caption won't show
-					if (!in_array("thumb", $parts)) {
-						$parts[] = "thumb";
+					if ( !in_array( 'thumb', $parts ) ) {
+						$parts[] = 'thumb';
 					}
 					// put it all back together and do the replacement
-					$img =  "[[" . implode("|", $parts) . "|" . $f . "]]";
-					$s = str_replace($images[0], $img, $s);
+					$img = '[[' . implode( '|', $parts ) . '|' . $f . ']]';
+					$s = str_replace( $images[0], $img, $s );
 					$changed = true;
 				}
 			}
 		}
 		// add it back to the new list
-		$newsteps[] = $s;
+		$newSteps[] = $s;
 	}
 
 	// short-circuit return
-	if (!$changed) {
+	if ( !$changed ) {
 		return true;
 	}
 
 	// put it all back together
-	$sections[$index] = implode("\n", $newsteps);
-	$text = implode("\n", $sections);
+	$sections[$index] = implode( "\n", $newSteps );
+	$text = implode( "\n", $sections );
+
 	return true;
 }
