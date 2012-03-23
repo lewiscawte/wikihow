0c2b440dc91735ab431e786b9440a2158e937239
Jack Phoenix <jack@countervandalism.net>
2012-03-23 22:27:02 +0000

    CategoryListing: *coding style tweaks *add code comments *add i18n file
    There *has* to be a better way to do the categorylisting-categorytable message...

--- wikiHow/extensions/CategoryListing/CategoryListing.body.php
+++ wikiHow/extensions/CategoryListing/CategoryListing.body.php
@@ -1,19 +1,36 @@
-<?
-class Categorylisting extends SpecialPage {
+<?php
 
-    function __construct($source = null) {
-        SpecialPage::SpecialPage( 'Categorylisting' );
-    }
+class CategoryListing extends SpecialPage {
 
-	function execute($par) {
+	/**
+	 * Constructor -- set up the new special page
+	 */
+	public function __construct() {
+		parent::__construct( 'CategoryListing' );
+	}
+
+	/**
+	 * Show the special page
+	 *
+	 * @param $par Mixed: parameter passed to the special page or null
+	 */
+	public function execute( $par ) {
 		global $wgOut;
 
+		// Set the page title...
 		$this->setHeaders();
-		$wgOut->setRobotpolicy('index,follow');
-		$wgOut->addHTML(wfMsg('categorylisting_subheader'));
-		$wgOut->addHTML(preg_replace('/\<[\/]?pre\>/', '', wfMsg( 'categorylisting_categorytable', wfGetPad() )));
+		// ...and overide the default robot policy as we want search engines
+		// to index this page
+		$wgOut->setRobotPolicy( 'index,follow' );
+
+		// Add the "learn how to categorize an article" message
+		$wgOut->addWikiMsg( 'categorylisting-subheader' ) );
+		// The actual content of the page; before displaying it, get rid of the
+		// surrounding pre tags
+		$wgOut->addHTML( preg_replace( '/\<[\/]?pre\>/', '',
+			wfMsg( 'categorylisting-categorytable', wfGetPad() )
+		) );
 
 		return;
-
 	}
 }
--- wikiHow/extensions/CategoryListing/CategoryListing.php
+++ wikiHow/extensions/CategoryListing/CategoryListing.php
@@ -1,27 +1,27 @@
 <?php
-if ( ! defined( 'MEDIAWIKI' ) )
-	die();
-    
-/**#@+
- * 
- * @package MediaWiki
- * @subpackage Extensions
+/**
+ * Adds a new special page showing a list of the topmost (root) categories.
  *
- * @link http://www.wikihow.com/WikiHow:Categorylisting-Extension Documentation
- *
- *
+ * @file
+ * @ingroup Extensions
+ * @version 1.0
  * @author Travis Derouin <travis@wikihow.com>
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die();
+}
 
-$wgShowRatings = true; // set this to false if you want your ratings hidden
-
-
+// Extension credits that will show up on Special:Version
 $wgExtensionCredits['specialpage'][] = array(
-    'name' => 'Categorylisting',
-    'author' => 'Travis <travis@wikihow.com>',
-	'description' => 'Provides a list of the top most categories in wikiHow', 
+	'name' => 'CategoryListing',
+	'version' => '1.0',
+	'author' => 'Travis Derouin',
+	'description' => 'Provides [[Special:CategoryListing|a list]] of the top most categories',
 );
 
-$wgSpecialPages['Categorylisting'] = 'Categorylisting';
-$wgAutoloadClasses['Categorylisting'] = dirname( __FILE__ ) . '/Categorylisting.body.php';
+// Set up the new special page
+$dir = dirname( __FILE__ ) . '/';
+$wgExtensionMessagesFiles['CategoryListing'] = $dir . 'CategoryListing.i18n.php';
+$wgAutoloadClasses['CategoryListing'] = $dir . 'CategoryListing.body.php';
+$wgSpecialPages['CategoryListing'] = 'CategoryListing';
