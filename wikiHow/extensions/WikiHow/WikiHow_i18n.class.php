8b6a7410e65b8094d76681c21d91679bf728859b
Jack Phoenix <jack@countervandalism.net>
2012-03-24 00:04:08 +0000

    minor cleanup to the WikiHow_i18n class; this file/class might be going away eventually in favor of using the ResourceLoader to load i18n messages for JS

--- wikiHow/extensions/WikiHow/WikiHow_i18n.class.php
+++ wikiHow/extensions/WikiHow/WikiHow_i18n.class.php
@@ -1,33 +1,32 @@
-<?
+<?php
 
 class WikiHow_i18n {
 
 	/**
-	 * Generates and returns a string of javascript that can be embedded in
-	 * your HTML to make Mediawiki messages available via the wfMsg() call.
+	 * Generates and returns a string of JavaScript that can be embedded in
+	 * your HTML to make MediaWiki messages available via the wfMsg() call.
 	 * Requires that jQuery is already loaded.
 	 *
 	 * Example usage:
 	 * <?php
-	 *   $langKeys = array('done-button', 'welcome', 'some-random-message-key');
-	 *   $js = WikiHow_i18n::genJSMsgs($langKeys);
+	 *   $langKeys = array( 'done-button', 'welcome', 'some-random-message-key' );
+	 *   $js = WikiHow_i18n::genJSMsgs( $langKeys );
 	 *   echo $js;
 	 * ?>
-	 * <script> alert('my message: ' + wfMsg('welcome')); </script>
+	 * <script> alert( 'my message: ' + wfMsg( 'welcome' ) ); </script>
 	 */
-	public static function genJSMsgs($langKeys) {
-
+	public static function genJSMsgs( $langKeys ) {
 		$js = "
 <script>
-	if (typeof WH == 'undefined') WH = {};
-	if (typeof WH.lang == 'undefined') WH.lang = {};
+	if ( typeof WH == 'undefined' ) WH = {};
+	if ( typeof WH.lang == 'undefined' ) WH.lang = {};
 	jQuery.extend(WH.lang, {
 ";
-		$len = count($langKeys);
-		foreach ($langKeys as $i => $key) {
-			$msg = preg_replace('@([\'\\\\])@', '\\\\$1', wfMsg($key));
-			$js .= "'$key': '$msg'" . ($i == $len - 1 ? '' : ',');
-			if ($i % 5 == 4 && $i < $len - 1) {
+		$len = count( $langKeys );
+		foreach ( $langKeys as $i => $key ) {
+			$msg = preg_replace( '@([\'\\\\])@', '\\\\$1', wfMsg( $key ) );
+			$js .= "'$key': '$msg'" . ( $i == $len - 1 ? '' : ',' );
+			if ( $i % 5 == 4 && $i < $len - 1 ) {
 				$js .= "\n";
 			}
 		}
@@ -39,8 +38,7 @@ class WikiHow_i18n {
 		return $js;
 	}
 
-	public static function genCSSMsgs($langKeys) {
+	public static function genCSSMsgs( $langKeys ) {
 		// TODO
 	}
 }
-
