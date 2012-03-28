838a2da59535c7bf7412451f6117153f7e8ecb59
Jack Phoenix <jack@countervandalism.net>
2012-03-28 19:57:39 +0000

    UserTalkTool: *rewrote JS to remove unwanted Prototype dependency, moved it into its own file and loaded it via ResourceLoader *introduced new 'usertalktool' user right and gave it to sysops by default instead of checking for the removed User::isSysop() method in the special page's execute() method *coding style tweaks *removed some unused variables *changed wfDebug calls to wfDebugLog calls *more and better i18n *added missing image file
    TODO/FIXME: added a FIXME comment in the JS file, because right now the JS both depends on the user's interface language (must be English at the moment) and the format of MediaWiki:Usertalktool-done. There should be a better way to check if sending the message was successful or not.

--- wikiHow/extensions/UserTalkTool/UserTalkTool.body.php
+++ wikiHow/extensions/UserTalkTool/UserTalkTool.body.php
@@ -2,247 +2,196 @@
 
 class UserTalkTool extends UnlistedSpecialPage {
 
-    function __construct() {
-        UnlistedSpecialPage::UnlistedSpecialPage( 'UserTalkTool' );
-    }
-	
-    function execute ($par) {
-		global $wgRequest, $wgUser, $wgOut, $wgLang, $wgServer;
- 
-		wfLoadExtensionMessages('UserTalkTool');		
+	/**
+	 * Constructor -- set up the new special page
+	 */
+	public function __construct() {
+		parent::__construct( 'UserTalkTool', 'usertalktool' );
+	}
 
-		// CHECK FOR ADMIN STATUS
-		if ( !in_array( 'sysop', $wgUser->getGroups() ) ) {
-			$wgOut->setArticleRelated( false );
-			$wgOut->setRobotpolicy( 'noindex,nofollow' );
-			$wgOut->errorpage( 'nosuchspecialpage', 'nospecialpagetext' );
+	/**
+	 * Show the special page
+	 *
+	 * @param $par Mixed: parameter passed to the page or null
+	 */
+	public function execute( $par ) {
+		global $wgRequest, $wgUser, $wgOut, $wgLang;
+
+		// Check that the user is allowed to access UserTalkTool
+		if ( !$wgUser->isAllowed( 'usertalktool' ) ) {
+			$this->displayRestrictionError();
 			return;
 		}
 
-		// MAKE SURE USER IS NOT BLOCKED
+		// Make sure that the user isn't blocked
 		if( $wgUser->isBlocked() ) {
 			$wgOut->blockedPage();
 			return;
 		}
-	
-		// CHECK FOR TARGET
+
+		// Check for target
 		$target = isset( $par ) ? $par : $wgRequest->getVal( 'target' );
-		if ($target == null || $target == "") {
-			$wgOut->addHTML('No target specified');
+		if ( $target == null || $target == '' ) {
+			$wgOut->addHTML( wfMsg( 'usertalktool-error-no-target' ) );
 			return;
 		}
 
+		$dbr = wfGetDB( DB_SLAVE );
 
-		$dbw = wfGetDB(DB_MASTER);
-		$dbr = wfGetDB(DB_SLAVE);
-		$me  = Title::makeTitle(NS_SPECIAL, "UserTalkTool");
+		// Process form
+		if ( $wgRequest->wasPosted() ) {
+			$wgOut->setArticleBodyOnly( true );
 
-		// PROCESS FORM
-		//
-		//
-		if ($wgRequest->wasPosted()) {
+			$utmsg = $wgRequest->getVal( 'utmessage' );
 
-			$wgOut->setArticleBodyOnly(true);
-
-			$utmsg = $wgRequest->getVal('utmessage');
-
-			if ($utmsg != "") {
-				#$t = Title::newFromID($aid);
+			if ( $utmsg != '' ) {
 				$ts = wfTimestampNow();
-			
+
 				$user = $wgUser->getName();
-				$real_name = User::whoIsReal($wgUser->getID());
-				if ($real_name == "") {
+				$real_name = User::whoIsReal( $wgUser->getID() );
+				if ( $real_name == '' ) {
 					$real_name = $user;
 				}
-				
 
-				//User
-				//
-				//
-				$utitem = $wgRequest->getVal('utuser');
-				wfDebug("UTT: posting user: $utitem\n");
-				wfDebug("UTT: by admin user: ". $wgUser->getID() ."\n");
-				
-				if ($utitem != "") {
+				// User
+				$utitem = $wgRequest->getVal( 'utuser' );
+				wfDebugLog( 'UserTalkTool', "posting user: $utitem" );
+				wfDebugLog( 'UserTalkTool', 'by admin user: ' . $wgUser->getID() );
 
-					// POST USER TALK PAGE
-					//
-					//
-					$text = "";
-					$aid = "";
-					$a = "";
-					$formattedComment = "";
+				if ( $utitem != '' ) {
+					// Post user talk page
+					$text = '';
+					$aid = '';
+					$a = '';
+					$formattedComment = '';
 
 					$u = new User();
-					$u->setName($utitem);
+					$u->setName( $utitem );
 					$user_talk = $u->getTalkPage();
 
-			      $dateStr = $wgLang->timeanddate(wfTimestampNow());
+					$dateStr = $wgLang->timeanddate( wfTimestampNow() );
 
-					$formattedComment = wfMsg('postcomment_formatted_comment', $dateStr, $user, $real_name, mysql_real_escape_string($utmsg));
+					$formattedComment = wfMsg(
+						'postcomment_formatted_comment',
+						$dateStr,
+						$user,
+						$real_name,
+						mysql_real_escape_string( $utmsg ) // @todo FIXME: this is awfully silly
+					);
 
 					$aid = $user_talk->getArticleId();
-					if ($aid > 0) {
-						$r = Revision::newFromTitle($user_talk);
+					if ( $aid > 0 ) {
+						$r = Revision::newFromTitle( $user_talk );
 						$text = $r->getText();
-					} 
-					$a = new Article($user_talk);
+					}
+					$a = new Article( $user_talk, 0 );
 					$text .= "\n\n$formattedComment\n\n";
 
-     				if ($aid > 0) {
-						$a->updateArticle($text, "", true, false, false, '', true);
+	 				if ( $aid > 0 ) {
+						$a->updateArticle( $text, '', true, false, false, '', true );
 					} else {
-						$a->insertNewArticle($text, "", true, false, true, false, false);
+						$article->doEdit( $value, '', ( EDIT_MINOR & EDIT_SUPPRESS_RC ) );
 					}
 
-					// MARK CHANGES PATROLLED
-					//
-					//
-					$res = $dbr->select('recentchanges', 'max(rc_id) as rc_id', array('rc_title=\''.mysql_real_escape_string($utitem).'\'', 'rc_user='.$wgUser->getID() ,'rc_cur_id=' . $aid, 'rc_patrolled=0'));
+					// Mark changes as patrolled
+					$res = $dbr->select(
+						'recentchanges',
+						'MAX(rc_id) AS rc_id',
+						array(
+							'rc_title' => $utitem,
+							'rc_user' => $wgUser->getID(),
+							'rc_cur_id' => $aid,
+							'rc_patrolled' => 0
+						),
+						__METHOD__
+					);
 
-  			      while ($row = $dbr->fetchObject($res)) {
-								wfDebug("UTT: mark patroll rcid: ".$row->rc_id ." \n");
-								RecentChange::markPatrolled( $row->rc_id );
-								PatrolLog::record( $row->rc_id, false );
+					foreach ( $res as $row ) {
+						wfDebugLog( 'UserTalkTool', 'mark patrol rcid: ' . $row->rc_id );
+						RecentChange::markPatrolled( $row->rc_id );
+						PatrolLog::record( $row->rc_id, false );
 					}
-					$dbr->freeResult($res);
 
-					wfDebug("UTT: done\n");
-					wfDebug("UTT: Completed posting for [".$utitem."]\n");
-					$wgOut->addHTML( "Completed posting for - ".$utitem);
-
+					wfDebugLog( 'UserTalkTool', 'Done. Completed posting for [' . $utitem . ']' );
+					$wgOut->addHTML( wfMsg( 'usertalktool-done', $utitem ) );
 				} else {
-					wfDebug("UTT: No user\n");
-					$wgOut->addHTML( "UT_MSG ERROR: No user specified. \n");
-
+					wfDebugLog( 'UserTalkTool', 'No user' );
+					$wgOut->addHTML( wfMsg( 'usertalktool-error-no-user' ) );
 				}
 			} else {
-				wfDebug("UTT: No message to post\n");
-				$wgOut->addHTML( "UT_MSG ERROR: No message to post for - ".$utitem."\n");
+				wfDebugLog( 'UserTalkTool', 'No message to post' );
+				$wgOut->addHTML( wfMsg( 'usertalktool-error-no-msg', $utitem ) );
 				return;
 			}
-			$wgOut->redirect('');
+
+			$wgOut->redirect( '' );
 		} else {
+			// Add JavaScript
+			$wgOut->addModules( 'ext.userTalkTool' );
 
-			$sk = $wgUser->getSkin();
-			$wgOut->addHTML('<style type="text/css" media="all">/*<![CDATA[*/ @import "/extensions/wikihow/newskin.css"; /*]]>*/</style>');
-			$wgOut->addHTML('
-<script language="javascript" src="/extensions/wikihow/prototype1.8.2/prototype.js"></script>
-<script language="javascript" src="/extensions/wikihow/prototype1.8.2/effects.js"></script>
-<script language="javascript" src="/extensions/wikihow/prototype1.8.2/controls.js"></script>
-		' . "\n");
+			// Define the variable to avoid PHP whining about undefined vars...
+			$utList = 0;
 
-
-			$wgOut->addHTML ( "
-			<script type='text/javascript'>
-				function utSend () {
-
-					$('formdiv').style.display = 'none';
-					$('resultdiv').innerHTML = 'Sending...<br />';
-
-					liArray = document.getElementById('ut_ol').childNodes;
-					i=0;
-					while(liArray[i]){
-						if (document.getElementById(liArray[i].id)) {
-							if (liArray[i].getAttribute('id').match(/^ut_li_/)) {
-
-								document.forms['utForm'].utuser.value = liArray[i].getAttribute('id').replace('ut_li_','');
-								$('utForm').request({
-									asynchronous: false,
-									onComplete: function(transport) {
-										$('resultdiv').innerHTML += transport.responseText+'<br />';
-										if (transport.responseText.match(/Completed posting for - /)){
-							
-											var u = transport.responseText.replace(/Completed posting for - /,'');
-											//$('resultdiv').innerHTML += 'UID: '+u+'<br />';
-											$('ut_li_'+u).innerHTML +=  '  <img src=\"/skins/WikiHow/light_green_check.png\" height=\"15\" width=\"15\" />';
-										}
-									},
-									onFailure: function(transport) {
-										$('resultdiv').innerHTML += 'Sending returned error for '+liArray[i].id+' <br />';
-									}
-								});
-
-								//$('resultdiv').innerHTML += 'Sending '+liArray[i].id+'<br />';
-						 	}
-						}
-						i++;
-
-					}
-					
-				return false;
-				}
-			</script>
-			
-			");
-
-
-			// GET LIST OF RECIPIENTS
-			//
-			//
-			if ($target) {
-				$t = Title::newFromUrl($target );
-				if ($t->getArticleId() <= 0) {
-					$wgOut->addHTML("Target not a valid article.");
+			// Get the list of recipients
+			if ( $target ) {
+				$t = Title::newFromUrl( $target );
+				if ( $t->getArticleId() <= 0 ) {
+					$wgOut->addHTML( wfMsg( 'usertalktool-error' ) );
 					return;
 				} else {
-					$r = Revision::newFromTitle($t);
+					$r = Revision::newFromTitle( $t );
 					$text = $r->getText();
-					#$wgOut->addHTML( $text );
 
-					$utcount = preg_match_all('/\[\[User_talk:(.*?)[#\]\|]/',$text,$matches);
-					#print_r($matches);
-					$utlist = $matches[1];
-
+					global $wgContLang;
+					$talkspaceName = $wgContLang->getNsText( NS_USER_TALK );
+					$utcount = preg_match_all(
+						'/\[\[' . str_replace( ' ', '_', $talkspaceName ) . ':(.*?)[#\]\|]/',
+						$text,
+						$matches
+					);
+					$utList = $matches[1];
 				}
 			}
 
-			// DISPLAY COUNT OF USER TALK PAGES FOUND
-			//
-			//
-			if (count($utlist) == 0) {
-				$wgOut->addHTML(wfMsg('notalkpagesfound'));
+			// Display amount of found user talk pages
+			if ( count( $utList ) == 0 ) {
+				$wgOut->addHTML( wfMsg( 'usertalktool-no-talk-pages-found' ) );
 				return;
 			} else {
-				$wgOut->addHTML(count($utlist) .' '.  wfMsg('talkpagesfound')."<br />");
+				$wgOut->addHTML( wfMsgExt( 'usertalktool-talkpagesfound', 'parsemag', count( $utList ) ) . '<br />' );
 			}
-			// TEXTAREA and FORM
-			//
-			//
-			$wgOut->addHTML('
-<form id="utForm" method="post">
-				');
 
-			// DISPLAY LIST OF USER TALK PAGES
-			//
-			//
-			$wgOut->addHTML('<div id="utlist" style="border: 1px grey solid;margin: 15px 0px 15px 0px;padding: 15px;height:215px;overflow:auto"><ol id="ut_ol">' . "\n");
-			foreach ($utlist as $utitem) {
-				$wgOut->addHTML('<li id="ut_li_'.preg_replace('/\s/m', '-', $utitem).'"><a href="/User_talk:' . $utitem . '">' . $utitem . '</a></li>' . "\n");
+			// Textarea and form
+			$wgOut->addHTML( '<form id="utForm" method="post">' );
+
+			// Display the list of user talk pages
+			$wgOut->addHTML(
+				'<div id="utlist" style="border: 1px grey solid; margin: 15px 0px 15px 0px; padding: 15px; height: 215px; overflow: auto">
+					<ol id="ut_ol">' . "\n"
+			);
+			foreach ( $utList as $utItem ) {
+				$wgOut->addHTML(
+					'<li id="ut_li_' . preg_replace( '/\s/m', '-', $utItem ) . '">' .
+						Linker::link( Title::newFromText( $utItem, NS_USER_TALK ), $utItem ) .
+					'</li>' . "\n"
+				);
 			}
-			$wgOut->addHTML('</ol></div>' . "\n");
+			$wgOut->addHTML( '</ol></div>' . "\n" );
 
-
-			// TEXTAREA and FORM
-			//
-			//
-			$wgOut->addHTML('
-<div id="formdiv">
-'. wfMsg('sendbox') .'<br />
+			// Textarea and form
+			$wgOut->addHTML(
+				'<div id="formdiv">' . wfMsg( 'usertalktool-sendbox' ) . '<br />
 <textarea id="utmessage" name="utmessage" rows="6" style="margin: 5px 0px 5px 0px;"></textarea>
-<input id="utuser" type="hidden" name="utuser" value="">
+<input id="utuser" type="hidden" name="utuser" value="" />
 
-<input tabindex="4" type="button" value="Send" cl1ass="btn" id="postcommentbutton" style="font-size: 110%; font-weight:bold" onclick="utSend(); return false;" />
-
+<input tabindex="4" type="button" value="' . wfMsg( 'usertalktool-send' ) . '" class="btn" id="postcommentbutton" style="font-size: 110%; font-weight: bold" />
 
 </form>
 </div>
-<div id="resultdiv"></div>' . "\n"); 
-
-
+<div id="resultdiv"></div>' . "\n"
+			);
 		}
 	}
 }
-	
+
 
--- wikiHow/extensions/UserTalkTool/UserTalkTool.php
+++ wikiHow/extensions/UserTalkTool/UserTalkTool.php
@@ -1,17 +1,41 @@
 <?php 
+/**
+ * UserTalkTool extension -- allows privileged users to post to multiple user
+ * talk pages at once
+ *
+ * @file
+ * @ingroup Extensions
+ * @version 1.0
+ * @author Vu Nguyen <vu@wikihow.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
 
 if ( !defined( 'MEDIAWIKI' ) ) {
-    exit(1);
+	exit( 1 );
 }
 
+// Extension credits that will show up on Special:Version
 $wgExtensionCredits['specialpage'][] = array(
-    'name' => 'UserTalkTool',
-    'author' => 'Vu Nguyen',
-    'description' => 'UserTalkTool will allow admin to post to multiple user talk pages at once',
+	'name' => 'UserTalkTool',
+	'version' => '1.0',
+	'author' => 'Vu Nguyen',
+	'description' => 'Allows privileged users to post to multiple user talk pages at once',
 );
 
-$wgExtensionMessagesFiles['UserTalkTool'] = dirname(__FILE__) . '/UserTalkTool.i18n.php';
+// ResourceLoader support for MediaWiki 1.17+
+$wgResourceModules['ext.userTalkTool'] = array(
+	'scripts' => 'UserTalkTool.js',
+	'messages' => array( 'usertalktool-sending', 'usertalktool-send-error' ),
+	'localBasePath' => dirname( __FILE__ ),
+	'remoteExtPath' => 'UserTalkTool'
+);
+
+// Set up the new special page
+$dir = dirname( __FILE__ ) . '/';
+$wgExtensionMessagesFiles['UserTalkTool'] = $dir . 'UserTalkTool.i18n.php';
+$wgAutoloadClasses['UserTalkTool'] = $dir . 'UserTalkTool.body.php';
 $wgSpecialPages['UserTalkTool'] = 'UserTalkTool';
-$wgAutoloadClasses['UserTalkTool'] = dirname( __FILE__ ) . '/UserTalkTool.body.php';
 
-
+// New user right, required to use the extension
+$wgAvailableRights[] = 'usertalktool';
+$wgGroupPermissions['sysop']['usertalktool'] = true;
