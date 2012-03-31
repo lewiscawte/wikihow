8a21eab201597e1e8cd671e61c71db8e116cdfb5
Jack Phoenix <jack@countervandalism.net>
2012-03-31 10:32:11 +0000

    MonitorPages: minor spacing tweak (x2) in the SQL file; absolutely no functional changes

--- wikiHow/extensions/MonitorPages/google_tables.sql
+++ wikiHow/extensions/MonitorPages/google_tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE /*_*/google_monitor (
   `gm_page` int(8) unsigned NOT NULL default '0',
   `gm_active` tinyint(1) unsigned NOT NULL default '1',
   UNIQUE KEY `gm_page` (`gm_page`)
- ) /*$wgDBTableOptions*/;
+) /*$wgDBTableOptions*/;
 
 --
 -- Table structure for table `google_monitor_results`
@@ -14,4 +14,4 @@ CREATE TABLE /*_*/google_monitor_results (
   `gmr_page` int(8) unsigned NOT NULL default '0',
   `gmr_position` tinyint(3) unsigned NOT NULL default '0',
   `gmr_timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP
- ) /*$wgDBTableOptions*/;
+) /*$wgDBTableOptions*/;
