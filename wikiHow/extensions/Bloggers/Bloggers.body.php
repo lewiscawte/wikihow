915035b08a3915e2ed4cc8028053f8c0a28f3245
Jack Phoenix <jack@countervandalism.net>
2012-03-31 21:59:32 +0000

    Bloggers: quick cleanup: *added version number to extension credits *added i18n file **moved the Google spreadsheet form key into the MediaWiki: namespace instead of hardcoding it in *changed short opening tags to standard PHP opening tags *coding style tweaks, as always

--- wikiHow/extensions/Bloggers/Bloggers.body.php
+++ wikiHow/extensions/Bloggers/Bloggers.body.php
@@ -1,29 +1,36 @@
-<?
-
-if (!defined('MEDIAWIKI')) die();
+<?php
 
 class Bloggers extends UnlistedSpecialPage {
 
-    public function __construct() {
-        UnlistedSpecialPage::UnlistedSpecialPage('Bloggers');
-    }
-
 	/**
-	 * The callback made to process and display the output of the 
-	 * Special:Bloggers page.
+	 * Constructor -- set up the new special page
 	 */
-    public function execute($par) {
-		global $wgOut, $wgRequest, $wgHooks;
-
-		$wgHooks['ShowBreadCrumbs'][] = array('Bloggers::removeBreadCrumbsCallback');
-
-		$wgOut->addHTML('<iframe src="https://spreadsheets.google.com/embeddedform?formkey=dHdUMlZ0a0p1SXM2NURDQTRvb0F3QVE6MQ" width="630" height="693" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>');
+	public function __construct() {
+		parent::__construct( 'Bloggers' );
 	}
 
-	public static function removeBreadCrumbsCallback(&$showBreadCrum) {
-		$showBreadCrum = false;
+	/**
+	 * The callback made to process and display the output of the
+	 * Special:Bloggers page.
+	 *
+	 * @param $par Mixed: parameter passed to the special page or null
+	 */
+	public function execute( $par ) {
+		global $wgHooks, $wgOut;
+
+		$wgHooks['ShowBreadCrumbs'][] = 'Bloggers::removeBreadCrumbsCallback';
+
+		$key = wfMessage( 'bloggers-formkey' )->inContentLanguage()->plain();
+		$wgOut->addHTML(
+			'<iframe src="https://spreadsheets.google.com/embeddedform?formkey=' . $key .
+			'" width="630" height="693" frameborder="0" marginheight="0" marginwidth="0">' .
+			wfMessage( 'bloggers-loading' )->plain() . '</iframe>'
+		);
+	}
+
+	public static function removeBreadCrumbsCallback( &$showBreadCrumb ) {
+		$showBreadCrumb = false;
 		return true;
 	}
 
 }
-
--- wikiHow/extensions/Bloggers/Bloggers.php
+++ wikiHow/extensions/Bloggers/Bloggers.php
@@ -1,26 +1,28 @@
-<?
-
-if (!defined('MEDIAWIKI')) die();
-    
-/**#@+
- * Display a google form/spreadsheet which people can use to sign up.
- * 
- * @package MediaWiki
- * @subpackage Extensions
+<?php
+/**
+ * Display a Google form/spreadsheet which people can use to sign up.
  *
- * @link http://www.wikihow.com/WikiHow:Bloggers-Extension Documentation
- *
+ * @file
+ * @ingroup Extensions
+ * @version 1.0
  * @author Reuben Smith <reuben@wikihow.com>
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die();
+}
+
+// Extension credits that will show up in Special:Version
 $wgExtensionCredits['special'][] = array(
 	'name' => 'Bloggers',
+	'version' => '1.0',
 	'author' => 'Reuben Smith',
-	'description' => 'Display a Google form for bloggers',
-	'url' => 'http://www.wikihow.com/WikiHow:Bloggers-Extension',
+	'description' => 'Display a Google form for bloggers which can be used to sign up'
 );
 
+// Set up the new special page
+$dir = dirname( __FILE__ ) . '/';
+$wgExtensionMessagesFiles['Bloggers'] = $dir . 'Bloggers.i18n.php';
+$wgAutoloadClasses['Bloggers'] = $dir . 'Bloggers.body.php';
 $wgSpecialPages['Bloggers'] = 'Bloggers';
-$wgAutoloadClasses['Bloggers'] = dirname( __FILE__ ) . '/Bloggers.body.php';
-
