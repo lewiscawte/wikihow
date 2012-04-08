0223e230c00ff592690194455e2dfa958bce794f
Jack Phoenix <jack@countervandalism.net>
2012-04-08 20:07:18 +0000

    StatsList: add i18n file, rewrite the SQL query to use proper Database methods and some general cleanup

---
+++ wikiHow/extensions/StatsList/StatsList.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalization file for the StatsList extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English */
+$messages['en'] = array(
+	'statslist' => 'Statistics about answered topic requests',
+	'statslist-requests-answered-24h' => 'Requests Answered (last 24 hours)',
+	'statslist-requests-answered-7d' => 'Requests Answered (7 days)',
+	'statslist-requests-answered-31d' => 'Requests Answered (last 31 days)',
+);
--- wikiHow/extensions/StatsList/StatsList.php
+++ wikiHow/extensions/StatsList/StatsList.php
@@ -1,17 +1,28 @@
 <?php
-
+/**
+ * StatsList extension -- shows statistics about answered topic requests
+ * See also the RequestTopic extension.
+ *
+ * @file
+ * @ingroup Extensions
+ * @version 1.0
+ * @author Bebeth Steudel
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
 if ( !defined( 'MEDIAWIKI' ) ) {
-    exit(1);
+	exit( 1 );
 }
 
+// Extension credits that will show up on Special:Version
 $wgExtensionCredits['specialpage'][] = array(
-    'name' => 'StatsList',
-    'author' => 'Bebeth <bebeth@wikihow.com>',
-    'description' => 'Just a bunch of stats for Krystle',
+	'name' => 'StatsList',
+	'version' => '1.0',
+	'author' => 'Bebeth Steudel',
+	'description' => 'Shows [[Special:StatsList|statistics]] about answered topic requests',
 );
 
-$wgSpecialPages['StatsList'] = 'StatsList';
-
-$dir = dirname(__FILE__) . '/';
-
+// Set up the new special page
+$dir = dirname( __FILE__ ) . '/';
+$wgExtensionMessagesFiles['StatsList'] = $dir . 'StatsList.i18n.php';
 $wgAutoloadClasses['StatsList'] = $dir . 'StatsList.body.php';
+$wgSpecialPages['StatsList'] = 'StatsList';
