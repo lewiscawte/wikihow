440f065c54c6e8988b5643dd1fbf2f67833c16d2
Jack Phoenix <jack@countervandalism.net>
2012-05-02 22:15:09 +0000

    rewrote checkPatrolCoach.php to use the Maintenance framework

--- wikiHow/extensions/MaintenanceScripts/checkPatrolCoach.php
+++ wikiHow/extensions/MaintenanceScripts/checkPatrolCoach.php
@@ -1,18 +1,54 @@
-<?
-require_once('commandLine.inc');
+<?php
+/**
+ * Checks which users have the Patrol Coach (RCTest extension) enabled
+ *
+ * @file
+ * @ingroup Maintenance
+ */
 
-$dbr = wfGetDB(DB_SLAVE);
+/**
+ * Set the correct include path for PHP so that we can run this script from
+ * $IP/extensions/MaintenanceScripts/ and we don't need to move this file to
+ * $IP/maintenance/.
+ */
+ini_set( 'include_path', dirname( __FILE__ ) . '/../../maintenance' );
 
-$res = $dbr->select('rctest_users', array('ru_user_id', 'ru_base_patrol_count'), array('ru_next_test_patrol_count' => 2, 'ru_base_patrol_count > 0'));
-while ($row = $dbr->fetchObject($res)) {
-	$u = User::newFromId($row->ru_user_id);
-	$u->load();
-	$enabled = RCTest::isEnabled($u->getId()) ? "on" : "off";
-	$base = $row->ru_base_patrol_count;
-	$wgUser = $u;
-	$total = $dbr->selectField('logging', 'count(*)', RCPatrolStandingsIndividual::getOpts());
-	$adjusted = $total - $base;
-	if ($adjusted > 4) {
-		echo "User: " . $u->getName() . ", preference: $enabled, adjusted: $adjusted\n";
+require_once( 'Maintenance.php' );
+
+class CheckPatrolCoach extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = 'Checks which users have the Patrol Coach (RCTest extension) enabled';
+	}
+
+	public function execute() {
+		$dbr = wfGetDB( DB_SLAVE );
+
+		$res = $dbr->select(
+			'rctest_users',
+			array( 'ru_user_id', 'ru_base_patrol_count' ),
+			array( 'ru_next_test_patrol_count' => 2, 'ru_base_patrol_count > 0' ),
+			__METHOD__
+		);
+		foreach ( $res as $row ) {
+			$u = User::newFromId( $row->ru_user_id );
+			$u->load();
+			$enabled = RCTest::isEnabled( $u->getId() ) ? 'on' : 'off';
+			$base = $row->ru_base_patrol_count;
+			$wgUser = $u;
+			$total = $dbr->selectField(
+				'logging',
+				'COUNT(*)',
+				RCPatrolStandingsIndividual::getOpts(),
+				__METHOD__
+			);
+			$adjusted = $total - $base;
+			if ( $adjusted > 4 ) {
+				$this->output( 'User: ' . $u->getName() . ", preference: $enabled, adjusted: $adjusted\n" );
+			}
+		}
 	}
 }
+
+$maintClass = 'CheckPatrolCoach';
+require_once( RUN_MAINTENANCE_IF_MAIN );
