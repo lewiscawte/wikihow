80a3470acf8a803e9c792a88011aee088bb25a45
Jack Phoenix <jack@countervandalism.net>
2013-01-06 15:53:26 +0000

    Importing wikihow-src-20110801.tar.gz

---
+++ maintenance/refreshLowRatings.php
@@ -0,0 +1,30 @@
+<?
+require_once( "commandLine.inc" );
+
+$dbw =& wfGetDB( DB_MASTER );
+$avg = wfMsg('list_bottom_rated_pages_avg');
+$minvotes = wfMsg('list_bottom_rated_pages_min_votes');
+$cleardays = wfMsg('list_bottom_rated_pages_clear_limit_days');
+
+$dateDiff = strtotime('now - 1 month');
+$res = $dbw->select(
+	array('rating'),
+	array('distinct rat_page'),
+	array('rat_deleted_when > FROM_UNIXTIME('.$dbw->addQuotes($dateDiff).')'),
+	'refreshLowRating.php:newlyDeleted');
+
+$newlyDeletedPages = array();
+while ($row = $dbw->fetchRow($res)) {
+	$newlyDeletedPages[] = $row['rat_page'];
+}
+
+$notin = join(',', $newlyDeletedPages);
+
+if (!empty($notin)) {
+	$notin = "and rat_page NOT IN (".$notin.")";
+}
+
+$dbw->query("delete from rating_low;", 'refreshLowRatings.php:delete');
+$dbw->query("insert into rating_low select rat_page, avg(rat_rating) as R, count(*) as C from rating, page where rat_page=page_id $notin and rat_isdeleted = 0 and page_is_redirect = 0 group by rat_page having R <= $avg and C >= $minvotes ; ",
+	'refreshLowRatings.php:refresh');
+?>
